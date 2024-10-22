f65c91d7117a61e8a8703331431ba958788fd405
Surya_Vetsa <110655337+Surya-Bh-ai@users.noreply.github.com>
2024-10-22 20:38:36 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull suryavetsa2330/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 suryavetsa2330/simple-python-flask-app
